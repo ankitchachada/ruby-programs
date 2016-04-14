1c5400f0249342ff383a6e7dabbecaa472598021
Ankit Chachada <ankit.chachada@gmail.com>
2016-04-14 09:20:24 -0700

    find the no of anagrams

---
+++ ruby programs/anagram.rb
@@ -0,0 +1,21 @@
+#find number of anagrams
+def factorial(n)
+ if n <= 1
+  return 1
+ else
+  return n*factorial(n-1)
+ end
+end
+puts "Enter a word.."
+a = gets.chomp
+
+h = Hash.new(0)
+a.chars.each do |i|
+ h[i] += 1
+end
+
+b = h.values.inject {|mem,i| mem*factorial(i)}
+
+ans = factorial(a.length)/b
+puts "There would #{ans} anagrams of #{a}"
+
